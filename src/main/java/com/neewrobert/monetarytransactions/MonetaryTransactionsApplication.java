245e497df463f8fc807b1fee35b2a24bb6062bcf
Newton Santos <neewrobert@gmail.com>
2020-10-20 17:01:13 -0300

    ïnit project

---
+++ src/main/java/com/neewrobert/monetarytransactions/MonetaryTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.neewrobert.monetarytransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetaryTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonetaryTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neewrobert/monetarytransactions/MonetaryTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neewrobert.monetarytransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonetaryTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
